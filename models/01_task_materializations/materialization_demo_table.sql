a43ba1101f312a806d4066d45a697f94017a29c8
karina.zozulia23@gmail.com <karina.zozulia23@gmail.com>
2025-12-11 22:48:18 +0000

    Added Task 1: Understanding Materializations - Views vs Tables

---
+++ models/01_task_materializations/materialization_demo_table.sql
@@ -0,0 +1,17 @@
+{{ config(materialized='table') }}
+
+with src as (
+    select * 
+    from SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER
+), 
+
+agg as (
+    select
+        c_nationkey,
+        count(*) as customer_count,
+        avg(c_acctbal) as avg_balance
+    from src
+    group by c_nationkey
+)
+
+select * from agg
